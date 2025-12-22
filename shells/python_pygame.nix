c76b47970259e49a2f0d6b244e02800ecea22983
def-not-jitesh <egnususiask765@gmail.com>
2025-12-22 16:06:54 +0530

    pygame shell config

--- shells/python_pygame.nix
+++ shells/python_pygame.nix
@@ -3,22 +3,14 @@ with import <nixpkgs> {};
 mkShell {
   buildInputs = [
     python310
-    python310Packages.pygame       # Pygame itself
-    SDL2                           # Core SDL3 library
-    SDL2_image                     # SDL3_image for PNG/JPG
-    SDL2_ttf                       # SDL3_ttf for fonts
-    SDL2_mixer                     # SDL3_mixer for audio
-    libpng                          # PNG support
-    libjpeg                         # JPEG support
-    freetype                        # Font support
+    python310Packages.pygame       
+    SDL2                           
+    SDL2_image                     
+    SDL2_ttf                       
+    SDL2_mixer                     
+    libpng                        
+    libjpeg                        
+    freetype                        
   ];
 
-  shellHook = ''
-    # Make sure SDL uses X11 (common on NixOS)
-    export SDL_VIDEODRIVER=x11
-    export DISPLAY=:0
-
-    echo "Pygame shell ready! Test with:"
-    echo "python -c 'import pygame; print(pygame.image.get_extended())'"
-  '';
 }
